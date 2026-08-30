16e38ea888968d10e508ae75cd75d39f7f2a6252
Jiahua Li <59403109+jiahualihuanahuan@users.noreply.github.com>
2026-08-30 00:34:16 -0400

    Update show_spinner in load_data cache decorator
    Changed show_spinner parameter from true to false in cache_data decorator.

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ max_vol_limit = st.sidebar.slider(
 # ---------------------------------------------------------
 # Data Fetching & Processing
 # ---------------------------------------------------------
-@st.cache_data(ttl="1d", show_spinner=true)
+@st.cache_data(ttl="1d", show_spinner=false)
 def load_data(symbols, start, end, is_max):
     # auto_adjust=True bakes dividends and splits into the 'Close' price 
     # to accurately reflect Total Return.
